3f18b591d485169312f141f011cf8e496232300a
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-03-15 11:22:34 -0400

    finished up the guided project

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,39 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { BASE_URL, API_KEY } from '../constants'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { BASE_URL, API_KEY } from "../constants";
+import axios from "axios";
 
 export default function Details(props) {
-  const { friendId, close } = props
-  const [details, setDetails] = useState(null)
+  const { friendId, close } = props;
+  const [details, setDetails] = useState(null);
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+  useEffect(() => {
+    console.log("Effect after the first DOM surgery");
+  }, []);
 
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
+  useEffect(() => {
+    console.log("Adding a silly event listener!");
+    const sillyClickListener = () => {
+      console.log("Here's a random number: " + Math.floor(Math.random() * 11));
+    };
+    document.addEventListener("click", sillyClickListener);
+
+    return () => {
+      console.log("Cleaning up silly event listener!");
+      document.removeEventListener("click", sillyClickListener);
+    };
+  }, []);
 
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+  useEffect(() => {
+    console.log("OH NO WHAT HAVE I DONE");
+  }, []);
 
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+      .then((res) => {
+        setDetails(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [friendId]);
 
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Details (of friend with id {friendId}):</h2>
-      {
-        details &&
+      {details && (
         <>
-          <p>{details.name} is {details.age}</p>
+          <p>
+            {details.name} is {details.age}
+          </p>
           <p>email is {details.email}</p>
           {name} likes:
           <ul>
-            {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
+            {details.hobbies.map((hobby) => (
+              <li key={hobby}>{hobby}</li>
+            ))}
           </ul>
         </>
-      }
+      )}
       <button onClick={close}>Close</button>
     </div>
-  )
+  );
 }
--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Friend = props => {
-    return (
-        <div className='friend'>
-            {props.info.name}
-            <button onClick={() => openDetails(props.info.id)}>
-                See details
-            </button>
-        </div>
-  )
-}
+const Friend = (props) => {
+  return (
+    <div className="friend">
+      {props.info.name}
+      <button onClick={() => props.openDetails(props.info.id)}>
+        See details
+      </button>
+    </div>
+  );
+};
 
 export default Friend;
